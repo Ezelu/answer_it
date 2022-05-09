bacaf479faa9cebb3ceb2aae12f8dd6f157252be
ezelu <ezelujhay@gmail.com>
2022-05-09 15:31:32 -0700

    Created the HeroPage and QuizPage components

---
+++ src/pages/HeroPage/HeroPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function HeroPage () {
+  return (
+    <div>
+      <h1> HeroPage </h1> 
+    </div>
+  )
+}
---
+++ src/pages/QuizPage/QuizPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function QuizPage () {
+  return (
+    <div>
+      <h1> Quiz Page </h1>
+    </div>
+  )
+}
