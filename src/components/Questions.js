d348f6fa62a64f3a58e202d55626c64c09ccc952
ezelu <ezelujhay@gmail.com>
2022-05-10 12:42:50 -0700

    Updated Questions

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,29 +1,59 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Container, Chip } from '@material-ui/core';
 import './Questions.css'
 
 
 
 export default function Question ({questions}) {
-  console.log(questions)
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
   return (
-    <Container className='info'>
-      <aside>
-        <h2> Category: </h2> 
-        <h4> {questions[0].category} </h4>
-      </aside>
+    <div>
+      <Container className='info'>
+        <div>
+          <h2> Category: </h2> 
+          <h4> {questions[0] && questions[0].category} </h4>
+        </div>
 
-      <aside>
-        <h2> difficulty: </h2> 
-        <h4> {questions[0].difficulty} </h4>
-      </aside>
+        <div>
+          <h2> difficulty: </h2> 
+          <h4> {questions[0] && questions[0].difficulty} </h4>
+        </div>
 
-      <aside>
-        <h2> Number Of Questions: </h2> 
-        <h4> {questions.length} </h4>
-      </aside>
+        <div>
+          <h2> Questions: </h2> 
+          <h4> {questions[0] && questions.length} </h4>
+        </div>
+      </Container>
 
 
-    </Container>
+      <Container>
+        <ol>
+          {
+            questions[0] &&
+
+            questions.map(each => {
+              return (
+                <li> {each.question} <br/>
+                  
+                </li>
+              )
+            })
+          }
+        </ol>
+      </Container>
+    </div>
   )
 }
