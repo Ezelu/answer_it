6c3a3f27ceb6e4ae3b1e7d4f32df1aa18e33975c
ezelu <ezelujhay@gmail.com>
2022-05-10 10:34:16 -0700

    Updated QuizPage

--- src/pages/QuizPage/QuizPage.js
+++ src/pages/QuizPage/QuizPage.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './QuizPage.css';
+import { Return } from '@mui/icons-material';
 import { Container } from '@material-ui/core';
+import Questions from '../../components/Questions';
 
 
 
@@ -10,18 +12,16 @@ import { Container } from '@material-ui/core';
 export default function QuizPage ({quiz_param}) {
 
   const { difficulty, type, category, number_of_questions } = quiz_param
-
-
   const [questions, set_questions] = useState([])
+  const [loading, set_loading] = useState(false)
 
   
   const fetchQuestions = async () => {
-    // const { data } = await axios.get(
-    //   `https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple`)
-
+    set_loading(true)
     const { data } = await axios.get(
-      `https://opentdb.com/api.php?amount=${number_of_questions}&category=${category}&difficulty=${difficulty}&type=${type}`)
-
+      `https://opentdb.com/api.php?amount=${number_of_questions}&category=${category}&difficulty=${difficulty}&type=${type}`);
+    
+    set_loading(false)
     set_questions(data.results);
   }
 
@@ -41,6 +41,8 @@ export default function QuizPage ({quiz_param}) {
     <Container className='container'>
       <h1> Quiz! </h1>
 
+
+      <Questions questions={questions} />
 
     </Container>
   )
