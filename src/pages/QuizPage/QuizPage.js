4e9ca577b3ed687e529052bafe7e27c76e687740
ezelu <ezelujhay@gmail.com>
2022-05-10 10:19:17 -0700

    Created the Quiz Page

--- src/pages/QuizPage/QuizPage.js
+++ src/pages/QuizPage/QuizPage.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './QuizPage.css';
+import { Container } from '@material-ui/core';
+
+
+
 
 
 export default function QuizPage ({quiz_param}) {
+
+  const { difficulty, type, category, number_of_questions } = quiz_param
+
+
+  const [questions, set_questions] = useState([])
+
+  
+  const fetchQuestions = async () => {
+    // const { data } = await axios.get(
+    //   `https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple`)
+
+    const { data } = await axios.get(
+      `https://opentdb.com/api.php?amount=${number_of_questions}&category=${category}&difficulty=${difficulty}&type=${type}`)
+
+    set_questions(data.results);
+  }
+
+
+  useEffect(() => {
+    fetchQuestions()
+  }, [])
+
+
+
+  console.log(questions)
+
+
+
+
   return (
-    <div>
-      <h1> Quiz Page {quiz_param.type} </h1>
+    <Container className='container'>
+      <h1> Quiz! </h1>
 
 
-    </div>
+    </Container>
   )
 }
